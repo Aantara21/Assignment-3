8ec8968551289d3e8444f390875acef6fb3355b2
Aantara21 <antarabaidya21@gmail.com>
2024-08-24 01:11:37 +0600

    header section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-tailwind.config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
